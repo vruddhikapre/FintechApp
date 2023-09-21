e52a35be20d3fcd4034f71c8fa40b2350dd2840d
Vruddhi Kapre <140089606+vruddhikapre@users.noreply.github.com>
2023-09-21 00:53:39 -0400

    Add files via upload

---
+++ HW/HW/Models/DataAnalysis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HW.Models
+{
+    /// <summary>
+    /// Added getters and setters for min,max, average
+    /// </summary>
+
+    public class DataAnalysis
+    {
+        public double min { get; set; }
+        public double max { get; set; }
+        public double average { get; set; }
+
+    }
+
+}
